83223ef9990abba7136c7f6a3b030f533d872f88
Jakub Micał <mc-soft@outlook.com>
2020-11-26 18:33:14 +0100

    Changed name web api and removed unnecessary code

---
+++ CompanyInventory.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyInventory.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        {
+        }
+
+        // [HttpGet]
+        // public IEnumerable<string> Get()
+        // {
+        //    
+        // }
+    }
+}
