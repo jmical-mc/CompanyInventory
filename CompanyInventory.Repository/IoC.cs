e4845abdc0d035848f606f64045a9f91a5c2aea3
Jakub Micał <mc-soft@outlook.com>
2020-11-27 22:30:43 +0100

    Added project repository and models

---
+++ CompanyInventory.Repository/IoC.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using CompanyInventory.Repository.Interfaces;
+
+namespace CompanyInventory.Repository
+{
+    public static class IoC 
+    {
+        public static void RegisterRepositories(this ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(ICompanyRepository).Assembly).AsImplementedInterfaces();
+        }
+    }
+}
